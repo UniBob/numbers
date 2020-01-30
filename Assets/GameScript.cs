3cfe2c83423f245b38d17ee586e291e6d0a93b63
UniBob <33136825+UniBob@users.noreply.github.com>
2020-01-30 19:16:51 +0300

    Initial commit

---
+++ Assets/GameScript.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+    int minNumber = 1;
+    int maxNumber = 1080;
+    int currentNumber;
+    int stepsAmount = 1;
+
+    bool access = true;
+
+    string hello = "Приветствую тебя, ";
+    string playerName = "путник";
+    string question = "Давай угадаю желаемое число?";
+    string min = "От ";
+    string max = " до ";
+    string variant = "Твоё число ";
+    string exit = "Хочешь закончить? Без проблем :)";
+    string newGame = "Давай начнём заново, ";
+    string steps = "Шагов понадобилось: ";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentNumber = (minNumber + maxNumber) / 2;
+        Debug.Log(hello + playerName+"!");
+        Debug.Log(question);
+        Debug.Log(min + minNumber.ToString() + max + maxNumber.ToString()+".");
+        Debug.Log(variant + currentNumber.ToString()+"?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (access)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                minNumber = currentNumber;
+                currentNumber = (minNumber + maxNumber) / 2;
+                stepsAmount++;
+                Debug.Log(variant + currentNumber.ToString() + "?");
+            }
+            else
+            {
+
+                if (Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    maxNumber = currentNumber;
+                    currentNumber = (minNumber + maxNumber) / 2;
+                    stepsAmount++;
+                    Debug.Log(variant + currentNumber.ToString() + "?");
+                }
+                else
+                {
+
+                    if (Input.GetKeyDown(KeyCode.Return))
+                    {
+                        Debug.Log("УГАДАЛ!!!");
+                        Debug.Log(steps + stepsAmount.ToString());
+                        access = false;
+                    }
+                }
+            }         
+        }
+
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                access = true;
+                minNumber = 1;
+                maxNumber = 1080;
+                stepsAmount = 1;
+                currentNumber = (minNumber + maxNumber) / 2;
+                Debug.Log(newGame + playerName + "!");
+                Debug.Log(question);
+                Debug.Log(min + minNumber.ToString() + max + maxNumber.ToString() + ".");
+                Debug.Log(variant + currentNumber.ToString() + "?");
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            access = false;
+            Debug.Log(exit);
+        }
+    }
+
+}
---
+++ Assets/SecondTask.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondTask : MonoBehaviour
+{
+    int sum = 0;
+    int stepsAmount = 0;
+    int sumLimit = 53;
+
+    string hello = "Сыграем в игру?";
+    string sumFrase = "Текущая сумма: ";
+    string finish = "Ты набрал нужную сумму. Поздравляю!";
+    string steps = "Количество шагов: ";
+
+    bool isGameRunning = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(hello);
+        Debug.Log(sumFrase + sum.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGameRunning)
+        {
+
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                sum++;
+                stepsAmount++;
+                if (sum<sumLimit) Debug.Log(sumFrase + sum.ToString());
+
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                sum+=2;
+                stepsAmount++;
+                if (sum < sumLimit) Debug.Log(sumFrase + sum.ToString());
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                sum+=3;
+                stepsAmount++;
+                if (sum < sumLimit) Debug.Log(sumFrase + sum.ToString());
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                sum+=4;
+                stepsAmount++;
+                if (sum < sumLimit) Debug.Log(sumFrase + sum.ToString());
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                sum+=5;
+                stepsAmount++;
+                if (sum < sumLimit) Debug.Log(sumFrase + sum.ToString());
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha6))
+            {
+                sum+=6;
+                stepsAmount++;
+                if (sum < sumLimit) Debug.Log(sumFrase + sum.ToString());
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha7))
+            {
+                sum+=7; 
+                stepsAmount++;
+                if (sum < sumLimit) Debug.Log(sumFrase + sum.ToString());
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha8))
+            {
+                sum+=8;
+                stepsAmount++;
+                if (sum < sumLimit) Debug.Log(sumFrase + sum.ToString());
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha9))
+            {
+                sum+=9;
+                stepsAmount++;
+                if (sum < sumLimit) Debug.Log(sumFrase + sum.ToString());
+            }
+            if (sum >= sumLimit)
+            {
+                Debug.Log(finish);
+                Debug.Log(steps + stepsAmount.ToString());
+                isGameRunning = false;
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isGameRunning = true;
+            sum = 0;
+            stepsAmount = 0;
+            Debug.Log(hello);
+            Debug.Log(sumFrase + sum.ToString());
+        }
+    }
+}
