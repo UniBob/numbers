7ae8f37723f6c0914f63ba66d3ed7a2408f7aa6d
UniBob <33136825+UniBob@users.noreply.github.com>
2020-02-04 21:16:59 +0300

    Denis's jokes

---
+++ Assets/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Player playerVar;
+    public int enemysCount = 10;
+    private string name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        name = playerVar.playerName;
+        Debug.Log("I'm your enemy, " + name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemysCount != 10)
+        {
+            Debug.Log(playerVar.playerName + " hit me!");
+            enemysCount = 10;
+        }
+        
+    }
+}
